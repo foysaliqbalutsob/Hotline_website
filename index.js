6156fdc97976c7bb03554afb8db7da0b1aba6401
my_first_github_repo <foysaliqbalutsob2202113>
2025-08-27 14:52:56 +0600

    card section done

---
+++ index.js
@@ -0,0 +1,70 @@
+function getElement(id){
+    const element = document.getElementById(id);
+    return element;
+
+}
+
+const btns = document.getElementsByClassName('card-btn');
+
+for(let button of btns){
+
+
+
+button.addEventListener('click', function(event){
+  
+    const  cardTitle = button.parentNode.parentNode.childNodes[3].innerText
+    const cardNumber = button.parentNode.parentNode.childNodes[7].innerText
+
+    console.log(cardTitle,cardNumber);
+
+    document.getElementById('history-container').innerHTML += `
+        <div class="h-[100px] rounded-lg bg-[var(--Surface,rgba(250,250,250,1))] p-4 mb-4 flex justify-between">
+        <div>
+            <h1 class=" text-[18px]">${cardTitle}</h1>
+            <h1 class ="text-[18px] text-[var(--Gray,rgba(92,92,92,1))]">${cardNumber}</h1>
+        </div>    
+        <div class="flex items-center">
+        <h1>time</h1>
+
+        </div>
+
+        </div>
+    `;
+    document.getElementById('history-container').appendChild(div);
+
+
+
+
+    
+
+    
+
+});
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    const clearButton = document.getElementById('refresh-btn');
+    clearButton.addEventListener('click', 
+
+
+        function(){
+            document.getElementById('history-container').innerHTML = '';
+        }
+    )
